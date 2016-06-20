f6836893f86cfb2b65079ee7ce32ac45c371f052
AnnaJ <an.juranova@gmail.com>
2016-06-20 11:59:46 -0400

    ipynb bar_model - Knl and Inl as functions

--- bar_model.py
+++ bar_model.py
@@ -20,4 +20,74 @@ class MWBarPotential(Potential):
     '''
     
     
-def __init__(self, amp=1.,.,)
+def __init__(self, amp=1.,.,):
+
+
+def _evaluate(self,R,theta,phi,t=0.):
+    """
+    NAME:
+        _evaluate
+    PURPOSE:
+        evaluate the potential at R, theta, phi
+    INPUT:
+        R - Galactcentric spherical radius
+        theta - 
+        phi - azimuth
+        t - time
+    OUTPUT:
+        Phi(R,theta,phi)
+    HISTORY:
+        2016-06-20 - Started - Juranova (MU)
+    """
+    return !!!!!!!!!!!!!
+
+def _Rforce(self,R,theta,phi,t=0.):
+    """
+    NAME:
+       _Rforce
+    PURPOSE:
+       evaluate the radial force for this potential
+    INPUT:
+       R - Galactocentric cylindrical radius
+       theta - 
+       phi - azimuth
+       t - time
+    OUTPUT:
+       the radial force
+    HISTORY:
+       2016-06-20 - Started - Juranova (MU)
+    """
+    return !!!!!!!!!!!!!
+    
+def _dens(self,R,z,phi=0.,t=0.):
+    """
+    NAME:
+        _dens
+    PURPOSE:
+        evaluate the density for this potential
+    INPUT:
+        R - Galactocentric cylindrical radius
+        z - vertical height
+        phi - azimuth
+        t - time
+    OUTPUT:
+        the density
+    HISTORY:
+       2016-06-20 - Started - Juranova (MU)
+    """
+def _R2deriv(self,R,z,phi=0.,t=0.):
+    """
+    NAME:
+        _R2deriv
+    PURPOSE:
+        evaluate the second radial derivative for this potential
+    INPUT:
+        R - Galactocentric cylindrical radius
+        z - vertical height
+        phi - azimuth
+        t - time
+    OUTPUT:
+        the second radial derivative
+    HISTORY:
+        2011-10-09 - Written - Bovy (IAS)
+    """
