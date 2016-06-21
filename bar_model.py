f81821345197beea2407e2f5ed8fa3f26d33e893
AnnaJ <an.juranova@gmail.com>
2016-06-21 15:48:39 -0400

    test.py for debugging bar_model.py added, bar_model.py edited

--- bar_model.py
+++ bar_model.py
@@ -32,6 +32,29 @@ def transform(r = False, theta = False, rho = False, z = False): # (r, theta, ph
     else:
         return 'please specify coordinates you wish to transform' # should be unnecessary later on
 
+Anlm = np.array([
+        [ 0,  0,  0,  1.509],
+        [ 1,  0,  0, -0.086],
+        [ 2,  0,  0, -0.033],
+        [ 3,  0,  0, -0.020],
+        [ 0,  2,  0, -2.606],
+        [ 1,  2,  0, -0.221],
+        [ 2,  2,  0, -0.001],
+        [ 0,  2,  2,  0.665],
+        [ 1,  2,  2,  0.129],
+        [ 2,  2,  2,  0.006],
+        [ 0,  4,  0,  6.406],
+        [ 1,  4,  0,  1.295],
+        [ 0,  4,  2, -0.660],
+        [ 1,  4,  2, -0.140],
+        [ 0,  4,  4,  0.044],
+        [ 1,  4,  4, -0.012],
+        [ 0,  6,  0, -5.859],
+        [ 0,  6,  2,  0.984],
+        [ 0,  6,  4, -0.030],
+        [ 0,  6,  6,  0.001],])
+        
+
 class MWBarPotential(Potential):
     """Class that implements bar potential presented in http://adsabs.harvard.edu/abs/2012MNRAS.427.1429W
     .. math::
@@ -45,7 +68,7 @@ class MWBarPotential(Potential):
            __init__
         PURPOSE:
            initialize Milky Way potential presented in http://adsabs.harvard.edu/abs/2012MNRAS.427.1429W
-        INPUT: ######## opravit!!!!!!!!!!!!!!!!!!!!!!!
+        INPUT: ######## needs corrections!!!!!!!!!!!!!!!!!!!!!!!
            #amp - amplitude to be applied to the potential (default: 1); can be a Quantity with units of mass or Gxmass
            xo, yo, zo - principal axes of the bar (xo = 1.49, yo = 0.58, zo = 0.40 in kpc)
            q - bulge axis ratio
@@ -59,11 +82,11 @@ class MWBarPotential(Potential):
         """
         Potential.__init__(self,amp=amp,ro=ro,vo=vo,amp_units='mass')
         if _APY_LOADED and isinstance(xo,units.Quantity):
-            xo= xo.to(units.kpc).value/self._ro
+            xo = xo.to(units.kpc).value/self._ro
         if _APY_LOADED and isinstance(yo,units.Quantity):
-            yo= yo.to(units.kpc).value/self._ro
+            yo = yo.to(units.kpc).value/self._ro
         if _APY_LOADED and isinstance(zo,units.Quantity):
-            zo= zo.to(units.kpc).value/self._ro
+            zo = zo.to(units.kpc).value/self._ro
         #### what to do with rhoo units???
         self._xo = xo
         self._yo = yo
@@ -75,50 +98,33 @@ class MWBarPotential(Potential):
                      and not isinstance(normalize,bool)):
             self.normalize(normalize)
                 
-        self.hasC= False
-        self.hasC_dxdv= False    
-        self._nemo_accname= 'MWBar'
+        self.hasC = False
+        self.hasC_dxdv = False    
+        self._nemo_accname = 'MWBar'
 
-    Anlm = np.array([
-            [ 0,  0,  0,  1.509],
-            [ 1,  0,  0, -0.086],
-            [ 2,  0,  0, -0.033],
-            [ 3,  0,  0, -0.020],
-            [ 0,  2,  0, -2.606],
-            [ 1,  2,  0, -0.221],
-            [ 2,  2,  0, -0.001],
-            [ 0,  2,  2,  0.665],
-            [ 1,  2,  2,  0.129],
-            [ 2,  2,  2,  0.006],
-            [ 0,  4,  0,  6.406],
-            [ 1,  4,  0,  1.295],
-            [ 0,  4,  2, -0.660],
-            [ 1,  4,  2, -0.140],
-            [ 0,  4,  4,  0.044],
-            [ 1,  4,  4, -0.012],
-            [ 0,  6,  0, -5.859],
-            [ 0,  6,  2,  0.984],
-            [ 0,  6,  4, -0.030],
-            [ 0,  6,  6,  0.001],])
+
 
     ##########
     def Knl(n,l):
+        print('now in Knl')
         return 0.5*n*(n + 4*l + 3) + (l + 1)*(2*l + 1)
 
     def Inl(n,l,m):
+        print('now in Inl')
         if m<=l:
             return(Knl(n,l)*1/(2**(8*l + 6))*gamma(n + 4*l + 3)/(factorial(n)*(n + 2*l + 3/2) *\
                 (gamma(2*l + 3/2))**2 )*(1 + diracdelta(m,0))*np.pi*2/(2*l + 1)*(factorial(l + m)/factorial(l - m)))
         else:
             return 'm must be smaller or equal to l' # just a temporary 'solution' for bad input
 
-    def Phinlm(n,l,m,r,phi,z):
+    def Phinlm(n,l,m,r,z,phi):
+        print('now in Phinlm')
         r, theta = transform(rho = r, z = z)
         s = r/1. # 1. is in kpc!!!!!
         return s**l/((1 + s)**(2*l + 1))*eval_gegenbauer(n, 2*l + 3/2, (s - 1)/(s + 1))*np.cos(m*phi)*lpmv(m, l, np.cos(theta))
     ##########
 
-    def _evaluate(self,R,phi,z,t=0.):
+    def _evaluate(self,R,z,phi, coeflist=Anlm, t=0.):
         """
         NAME:
             _evaluate
@@ -135,9 +141,12 @@ class MWBarPotential(Potential):
             2016-06-20 - Started - Juranova (MU)
         """
         # the constant (GM_{bar}/r_s) is set to one... but is that correct...? Now I don't think so...
+        print('now in _evaluate')
         Phi = 0
         for i in range(len(Anlm)):
-            Phi += Anlm[i,3]*Phinlm(Anlm[i,0], Anlm[i,1], Anlm[i,2], R, phi, z)
+            print(Anlm[i,0], Anlm[i,1], Anlm[i,2], R, z, phi)
+            print(type(Anlm[i,0]), type(Anlm[i,1]), type(Anlm[i,2]), type(R), type(z), type(phi))
+            Phi += Anlm[i,3]*self.Phinlm(Anlm[i,0], Anlm[i,1], Anlm[i,2], R, z, phi)
         return Phi
 '''
     def _Rforce(self,R,theta,phi,t=0.):
