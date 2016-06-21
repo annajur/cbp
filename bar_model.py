788bf955f04faaf26a535baf9bc8125fca05244a
AnnaJ <an.juranova@gmail.com>
2016-06-21 15:02:47 -0400

    template with MNP and testing ipynb added, bar_model.py edited

--- bar_model.py
+++ bar_model.py
@@ -2,12 +2,11 @@
 # Numpy imported as np!!
 # Even I have imported the division from __future__, I still use dots to
 #   force the result in float data type
-# Units still need to be converted to natural units
+# Units still need to be converted to natural units (rhoo, )
 #
 ######################################################################
-import numpy as np
 from __future__ import division
-
+import numpy as np
 from scipy.special import gamma
 from math import factorial
 from scipy.special import gegenbauer
@@ -25,13 +24,21 @@ def diracdelta(m,n):
     else:
         return 0
 
+def transform(r = False, theta = False, rho = False, z = False): # (r, theta, phi) <-> (rho, phi, z)
+    if not theta and not r:
+        return (np.sqrt(rho**2 + z**2), np.arccos(z/np.sqrt(rho**2 + z**2))) # (rho, z) transformed to (r, theta)
+    elif not z and not rho:
+        return(r*np.sin(theta), r*np.cos(theta)) # (r, theta) transformed to (rho, z)
+    else:
+        return 'please specify coordinates you wish to transform' # should be unnecessary later on
+
 class MWBarPotential(Potential):
     """Class that implements bar potential presented in http://adsabs.harvard.edu/abs/2012MNRAS.427.1429W
     .. math::
-        
+       ################## 
     """
     
-    def __init__(self, amp=1.,.,ro = None, vo = None): #####rozdelane, dokumentace okopirovana
+    def __init__(self, amp=1., xo=1.49, yo=0.58, zo=0.40, q=0.6, rhoo=1, ro=None, vo=None, normalize=False): #####rozdelane, dokumentace okopirovana
        
         """
         NAME:
@@ -39,9 +46,10 @@ class MWBarPotential(Potential):
         PURPOSE:
            initialize Milky Way potential presented in http://adsabs.harvard.edu/abs/2012MNRAS.427.1429W
         INPUT: ######## opravit!!!!!!!!!!!!!!!!!!!!!!!
-           amp - amplitude to be applied to the potential (default: 1); can be a Quantity with units of mass or Gxmass
-           a - scale length (can be Quantity)
-           b - scale height (can be Quantity)
+           #amp - amplitude to be applied to the potential (default: 1); can be a Quantity with units of mass or Gxmass
+           xo, yo, zo - principal axes of the bar (xo = 1.49, yo = 0.58, zo = 0.40 in kpc)
+           q - bulge axis ratio
+           rhoo - central density; determined by normalising the total mass of the bar, M_{bar} = 2.0*10^10 M_{sun}
            normalize - if True, normalize such that vc(1.,0.)=1., or, if given as a number, such that the force is this fraction of the force necessary to make vc(1.,0.)=1.
            ro=, vo= distance and velocity scales for translation into internal units (default from configuration file)
         OUTPUT:
@@ -49,20 +57,27 @@ class MWBarPotential(Potential):
         HISTORY:
            2016-06-20 - Started - Bovy (NYU)
         """
-        
+        Potential.__init__(self,amp=amp,ro=ro,vo=vo,amp_units='mass')
+        if _APY_LOADED and isinstance(xo,units.Quantity):
+            xo= xo.to(units.kpc).value/self._ro
+        if _APY_LOADED and isinstance(yo,units.Quantity):
+            yo= yo.to(units.kpc).value/self._ro
+        if _APY_LOADED and isinstance(zo,units.Quantity):
+            zo= zo.to(units.kpc).value/self._ro
+        #### what to do with rhoo units???
+        self._xo = xo
+        self._yo = yo
+        self._zo = zo
+        self._q = q
+        self._rhoo = rhoo
+        if normalize or \
+                (isinstance(normalize,(int,float)) \
+                     and not isinstance(normalize,bool)):
+            self.normalize(normalize)
+                
         self.hasC= False
         self.hasC_dxdv= False    
         self._nemo_accname= 'MWBar'
-
-    ##########
-    def transform(self, r = False, theta = False, rho = False, z = False): # (r, theta, phi) <-> (rho, phi, z)
-        if not theta and not r:
-            return(np.sqrt(rho**2 + z**2), np.arccos(z/np.sqrt(rho**2 + z**2))) # (rho, z) transformed to (r, theta)
-        elif not z and not rho:
-            return(r*np.sin(theta), r*np.cos(theta)) # (r, theta) transformed to (rho, z)
-        else:
-            return('please specify coordinates you wish to transform') # should be unnecessary later on
-    ##########
 
     Anlm = np.array([
             [ 0,  0,  0,  1.509],
@@ -88,20 +103,19 @@ class MWBarPotential(Potential):
 
     ##########
     def Knl(n,l):
-        return(0.5*n*(n + 4*l + 3) + (l + 1)*(2*l + 1))
+        return 0.5*n*(n + 4*l + 3) + (l + 1)*(2*l + 1)
 
     def Inl(n,l,m):
         if m<=l:
             return(Knl(n,l)*1/(2**(8*l + 6))*gamma(n + 4*l + 3)/(factorial(n)*(n + 2*l + 3/2) *\
                 (gamma(2*l + 3/2))**2 )*(1 + diracdelta(m,0))*np.pi*2/(2*l + 1)*(factorial(l + m)/factorial(l - m)))
         else:
-            return('m must be smaller or equal to l') # just a temporary solution for bad input
+            return 'm must be smaller or equal to l' # just a temporary 'solution' for bad input
 
     def Phinlm(n,l,m,r,phi,z):
         r, theta = transform(rho = r, z = z)
         s = r/1. # 1. is in kpc!!!!!
-        return(s**l/((1 + s)**(2*l + 1))*eval_gegenbauer(n, 2*l + 3/2, (s - 1)/(s + 1))*np.cos(m*phi)*lpmv(m, l, np.cos(theta)))
-    print(Phinlm(1,3,1,3,1,1))
+        return s**l/((1 + s)**(2*l + 1))*eval_gegenbauer(n, 2*l + 3/2, (s - 1)/(s + 1))*np.cos(m*phi)*lpmv(m, l, np.cos(theta))
     ##########
 
     def _evaluate(self,R,phi,z,t=0.):
@@ -124,8 +138,8 @@ class MWBarPotential(Potential):
         Phi = 0
         for i in range(len(Anlm)):
             Phi += Anlm[i,3]*Phinlm(Anlm[i,0], Anlm[i,1], Anlm[i,2], R, phi, z)
-        return(Phi)
-
+        return Phi
+'''
     def _Rforce(self,R,theta,phi,t=0.):
         """
         NAME:
@@ -142,9 +156,9 @@ class MWBarPotential(Potential):
         HISTORY:
         2016-06-20 - Started - Juranova (MU)
         """
-        return !!!!!!!!!!!!!
+        return None #!!!!!!!!!!!!!
         
-    def _dens(self, R, phi, z, t=0., x_0 = 1.49, y_0 = 0.58, z_0 = 0.40, q = 0.6, rho_0 = 1): # units!!!!!
+    def _dens(self, R, phi, z, t=0.): # units of rhoo!!!!!
         """
         NAME:
             _dens
@@ -155,11 +169,6 @@ class MWBarPotential(Potential):
             z - vertical height
             phi - azimuth
             t - time
-            x_0 - ##########
-            y_0 - ##########
-            z_0 - ##########
-            q - #########
-            rho_0 - ##########
         OUTPUT:
             the density
         HISTORY:
@@ -167,9 +176,9 @@ class MWBarPotential(Potential):
         """
         x = R*np.cos(phi)
         y = R*np.sin(phi)
-        r_1 = (((x/x_0)**2 + (y/y_0)**2)**2 + (z/z_0)**4)**0.25
-        r_2 = ((q**2*(x**2 + y**2) + z**2)/z_0**2)**0.5
-        return(rho_0*(np.exp(-r_1[i]**2/2.) + r_2[i]**(-1.85)*np.exp(-r_2[i])))
+        r_1 = (((x/self._xo)**2 + (y/self._yo)**2)**2 + (z/self._zo)**4)**0.25
+        r_2 = ((self._q**2*(x**2 + y**2) + z**2)/self._zo**2)**0.5
+        return self._rhoo*(np.exp(-r_1[i]**2/2.) + r_2[i]**(-1.85)*np.exp(-r_2[i]))
         
     def _R2deriv(self,R,z,phi=0.,t=0.):
         """
@@ -187,3 +196,5 @@ class MWBarPotential(Potential):
         HISTORY:
             2011-10-09 - Written - Bovy (IAS)
         """
+        return None #!!!!!!!!!!!!!!!
+'''
